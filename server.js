f515023d82b35ebab20a235778d232cd577fff1f
Метапротокол 2025 <metaprotocol2025@gmail.com>
2025-09-17 15:22:40 +0300

    Fix file structure: move index.html to root directory

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Store active rooms
 const rooms = new Map();
@@ -165,7 +165,7 @@ io.on('connection', (socket) => {
 
 // Serve the main page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Health check endpoint
